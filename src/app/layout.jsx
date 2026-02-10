07b7f336d29980bbf98d8497024e8973fb9b5688
martavukovic <martavukovic0@gmail.com>
2026-02-10 22:22:23 +0100

    Refreshing homepage

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,10 +1,19 @@
 import Navbar from "./components/navbar/navbar";
 import "./Home.module.css"
 import "./globals.css";
+import Head from 'next/head'
+
+export const metadata = {
+  title: 'Velmot',
+  description: 'Industrial Electrical Services',
+}
 
 export default function RootLayout({ children }) {
   return (
     <html lang="hr">
+      <Head>
+        <link rel="icon" href="/images/Velmot_cut_logo.png" />
+      </Head>
       <body>
         <Navbar />
         <main>{children}</main>
